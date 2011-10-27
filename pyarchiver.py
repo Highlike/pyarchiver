e8f4a5a8e0aeb4923b46c81295c28fcab10aec10
Kevin Geistdörfer <Kevin1995G@gmail.com>
2011-10-28 00:22:34 +0200

    archive navigation done

--- pyarchiver.py
+++ pyarchiver.py
@@ -3,6 +3,7 @@
 import tarfile
 import zipfile
 import sys
+import curses
 
 def determine_filetype(name):
     if tarfile.is_tarfile(name):
@@ -11,14 +12,6 @@ def determine_filetype(name):
         return "is_zip"
     else:
         raise ValueError("Not a valid archive type")
-
-def tar(name):
-    with tarfile.open(name) as file:
-        tar_file_list(file)
-
-def zip(name):
-    with zipfile.open(name) as file:
-        zip_file_list(file)
 
 def tar_file_list(file, dir):
     if not isinstance(dir, str):
@@ -36,12 +29,62 @@ def tar_file_list(file, dir):
         if len(item_list) == current_level:
             if item.startswith(dir):
                 new_dir.append(item_list[-1])
-    return sorted(new_dir)
+    if len(new_dir) ==  0:
+        raise ValueError("Not a valid directory")
+    else:
+        return sorted(new_dir)
 
+### GUI ###
+def user_interaction(win, y, dir_list, screen, archive):
+    c = 0
+    while not c == ord('l'):
+        c = win.getch()
+        if c == ord('j') and y < len(dir_list):
+            win.move(y, 1)
+            win.addstr('   ')
+            y += 1
+            win.move(y, 1)
+            win.addstr("-->")
+            win.refresh()
+        elif c == ord('k') and y > 1:
+            win.move(y, 1)
+            win.addstr('   ')
+            y -= 1
+            win.move(y, 1)
+            win.addstr("-->")
+            win.refresh()
+        elif c == ord('l'):
+            win.erase()
+            dir = dir_list[y - 1]
+            initialize(screen, archive, dir)
+
+def initialize(screen, archive, dir=""):
+    win = curses.newwin(10, 90)
+
+    try:
+        dir_list = tar_file_list(archive, dir)
+    except ValueError:
+        dir_list = ["Not a valid directory"]
+    x = 0
+    for item in dir_list:
+        x += 1
+        win.move(x, 6)
+        win.addstr(str(item))
+
+        y = 1
+        win.move(y, 1)
+        win.addstr("-->")
+
+        win.refresh
+
+    user_interaction(win, y, dir_list, screen, archive)
+   
 if __name__ == '__main__':
     script, file = sys.argv
     archive = determine_filetype(file)
     if archive == "is_tar":
-        tar(file)
+        with tarfile.open(file) as f:
+            curses.wrapper(initialize, f)
     elif archive == "is_zip":
-        zip(file)
+        with zipfile.open(file) as f:
+            curses.wrapper(initialize, f)
